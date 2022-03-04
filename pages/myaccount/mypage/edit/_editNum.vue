e9d9c2b7fa954f689ac884899636807f7fc7433b
Ki-Linux <linuxseima@gmail.com>
2022-03-04 22:08:52 +0900

    edit

---
+++ pages/myaccount/mypage/edit/_editNum.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="edit">
+        <form>
+            <input type="file" name="picture" ref="preview" @change="editPicture" required>
+            <img :src="url" alt="">
+        </form>
+    </div>
+</template>
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component
+export default class edit extends Vue {
+    url = "";
+    
+    editPicture(e: Event) {
+
+        const  file = (<HTMLInputElement>e.target).files![0];
+        this.url = URL.createObjectURL(file);
+
+    }
+
+}
+</script>
