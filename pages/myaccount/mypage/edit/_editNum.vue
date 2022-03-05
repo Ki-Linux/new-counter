bcd4559f3ea1915abbf95619b42c97dcf3bc32dc
Ki-Linux <linuxseima@gmail.com>
2022-03-05 18:52:08 +0900

    new post

--- pages/myaccount/mypage/edit/_editNum.vue
+++ pages/myaccount/mypage/edit/_editNum.vue
@@ -1,8 +1,16 @@
 <template>
     <div id="edit">
-        <form>
-            <input type="file" name="picture" ref="preview" @change="editPicture" required>
-            <img :src="url" alt="">
+        <form @submit.prevent="dataSend">
+            <div class="post_data">
+                <img :src="url" alt="">
+                <input type="file" name="picture" ref="preview" @change="editPicture" multiple>
+                <textarea name="comment" id="" cols="30" rows="10" maxlength="200" v-model="my_comment"></textarea>
+            </div>
+            <div class="content_data"  v-for="(content_data, index) in contentData" :key="content_data">
+                <input type="checkbox" id="check" name="selector" value="check" @change="checkOn(index)">
+                <label for="check">{{ content_data }}</label>
+            </div>
+            <button type="submit">{{ button_name }}</button>
         </form>
     </div>
 </template>
@@ -12,6 +20,28 @@ import { Vue, Component } from 'vue-property-decorator';
 @Component
 export default class edit extends Vue {
     url = "";
+    my_comment = "";
+    array_check: number[] = [0, 0, 0, 0];
+    //check: boolean = false;
+    contentData: string[] = [
+                            '他の人がいいねできるようにする', 
+                            'ほかのひとがコメントできるようにする', 
+                            'この投稿の閲覧数を投稿する', 
+                            'このサイトのトップページに表示する(チェックすることでこの投稿をより多くの人に見てもらえます。)'
+                        ];
+    button_name: string = "post";
+
+    mounted() {
+        if(this.$route.params.editNum === '1') {
+
+            this.button_name = "投稿";
+
+        } else {
+
+            this.button_name = "編集";
+
+        }
+    }
     
     editPicture(e: Event) {
 
@@ -20,5 +50,55 @@ export default class edit extends Vue {
 
     }
 
+    checkOn(index: number) {
+
+       
+            for(let i: number=0; i < 4; i++) {//ボックス一覧の表示
+
+                switch(index) {//チェックしている番号の列
+
+                case i:
+                    if(this.array_check[i] === 1) {//チェックしていないとき
+
+                        this.array_check[i] = 0;
+
+                    } else {
+
+                        this.array_check[i] = 1;
+
+                    }
+                break;
+
+            }
+            
+        }
+
+        //return array_check;
+
+    }
+
+    dataSend() {//databaseへ
+        
+        //console.log(this.array_check);
+
+        this.$axios.post("edit", {
+            username: 'hou',
+            image: this.url,
+            comment: this.my_comment,
+            show_good: this.array_check[0],
+            others_comment: this.array_check[1],
+            can_see: this.array_check[2],
+            to_top: this.array_check[3],
+        })
+        .then((response) => {
+            console.log(response);
+        })
+    }
+
 }
 </script>
+<style lang="scss">
+input {
+    display: block;
+}
+</style>
