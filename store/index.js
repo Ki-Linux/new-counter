dc1d7c3be29578d7f35c9e1334a8de8fe6eb09b8
Ki-Linux <linuxseima@gmail.com>
2022-03-28 11:20:27 +0900

    kokomodoru

--- store/index.js
+++ store/index.js
@@ -44,7 +44,7 @@ export const mutations = {
 
                 }
 
-                state.back_data.splice(2, 1, state.back_data[2]);
+                state.back_data.splice(2, 1, state.back_data[2]+=1);
                 
             
             
