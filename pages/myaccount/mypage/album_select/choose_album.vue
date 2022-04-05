02ec0fab0d560b661f07c18a8eae61e97375916c
Ki-Linux <linuxseima@gmail.com>
2022-04-05 22:59:17 +0900

    js complete

--- pages/myaccount/mypage/album_select/choose_album.vue
+++ pages/myaccount/mypage/album_select/choose_album.vue
@@ -1,18 +1,22 @@
 <template>
-    <div id="all_select_img" @submit.prevent="postData">
-        <div class="name">
+    <div id="all_select_img">
+        <form @submit.prevent="postData">
+            <div class="name">
             <input type="text" v-model="written_name" placeholder="タイトルを記入" maxlength="15">
         </div>
         <div class="img_box">
             <div class="show_img">
                 <img :src="img_data" alt="select_img">
             </div>
-            <div class="select_img" v-for="(next_img, index_num) in next_imgs" :key="index_num">
-                <button type="button" @click="nextImg(index_num)">{{ next_img }}</button>
+            <div class="select_img" v-if="$store.state.select_plan !== 'free'">
+                <p v-for="(next_img, index_num) in next_imgs" :key="index_num">
+                    <button type="button" @click="nextImg(index_num)">{{ next_img }}</button>
+                </p>
             </div>
             <div class="img_button">
                 <input type="file" name="picture" ref="preview" @change="editImg" multiple="multiple">
             </div>
+            <p>{{ attention }}</p>
             <!--<div class="no_img">
                 <button type="button">画像なし</button>
             </div>-->
@@ -21,6 +25,7 @@
             <button type="submit">アルバムに追加</button>
             <nuxt-link to="">アルバムを作成しない</nuxt-link>
         </div>
+        </form>  
     </div>
 </template>
 <script lang="ts">
@@ -32,6 +37,7 @@ export default class chooseAlbum extends Vue {
     img_num: number = 0;
     next_imgs: string[] = ["◀", "▶"];
     written_name: string = "";
+    attention: string = "";
 
     mounted() {
 
@@ -95,6 +101,36 @@ export default class chooseAlbum extends Vue {
     editImg(e: Event) {
         const  file = (<HTMLInputElement>e.target).files![0];
         this.img_data = URL.createObjectURL(file);
+    }
+
+    postData() {
+
+        
+
+        if(this.written_name === "" || this.img_data  === require('../../../../static/edit/hatena.png')){
+
+            this.attention = "タイトルまたは画像がありません。";
+            return;
+
+        }
+
+        const store_data = this.$store.state
+
+        this.$axios.post('album_data', {
+            username: store_data.username,
+            image: this.img_data,
+            selector: store_data.back_data[0],
+            target: store_data.back_data[1],
+            present: store_data.back_data[2],
+            title: this.written_name,
+        })
+        .then((response)=> {
+            console.log(response)
+        })
+
+
+
+
     }
 
 }
