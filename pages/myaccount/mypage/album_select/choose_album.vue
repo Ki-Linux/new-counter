0845f5780b274d83e9d101ea21e9ec45635b0a5e
Ki-Linux <linuxseima@gmail.com>
2022-04-05 21:16:33 +0900

    add htnl info

--- pages/myaccount/mypage/album_select/choose_album.vue
+++ pages/myaccount/mypage/album_select/choose_album.vue
@@ -1,10 +1,25 @@
 <template>
     <div id="all_select_img">
-        <div class="show_img" v-for="img_data in imgs_data" :key="img_data">
-            <img :src="img_data" alt="select_img">
+        <div class="name">
+            <input type="text">
         </div>
-        <div class="select_img">
-
+        <div class="img_box">
+            <div class="show_img">
+                <img :src="img_data[img_num]" alt="select_img">
+            </div>
+            <div class="select_img" v-for="(next_img, index_num) in next_imgs" :key="index_num">
+                <button>{{ next_img }}</button>
+            </div>
+            <div class="img_button">
+                <input type="file" name="picture" ref="preview" @change="editImg" multiple="multiple">
+            </div>
+            <div class="no_img">
+                <button>画像なし</button>
+            </div>
+        </div>
+        <div class="submit_button">
+            <button>アルバムに追加</button>
+            <nuxt-link to="">アルバムを作成しない</nuxt-link>
         </div>
     </div>
 </template>
@@ -13,7 +28,9 @@ import { Vue, Component } from 'vue-property-decorator';
 
 @Component
 export default class chooseAlbum extends Vue {
-    imgs_data: string[] = [require('../../../../static/edit/hatena.png')];
+    img_data: string[] = [require('../../../../static/edit/hatena.png')];
+    img_num: number = 0;
+    next_imgs: string[] = ["◀", "▶"];
 
     mounted() {
 
@@ -25,12 +42,12 @@ export default class chooseAlbum extends Vue {
 
             if(store.select_plan === "free"){
 
-                this.imgs_data.splice(0, 1, back_data[3]);
+                this.img_data.splice(0, 1, back_data[3]);
 
             } else {
 
                 for(let i=0; i < store.back_select_data.length; i++) {
-                    this.imgs_data.splice(i, 1, store.back_select_data[i]);
+                    this.img_data.splice(i, 1, store.back_select_data[i]);
                 }
                 
             }
